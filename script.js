75a787e7be6ab724444524ab29ec083928eb274d
irinaravenova <hernandezirina23@gmail.com>
2023-06-26 18:49:07 -0400

    user clicks button and plays 5 rounds against computer. user entry is also validated.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // rock paper scissors
 
-// RULES
+// RULES OF THE GAME
     // rock beats scissors
     // scissors beats paper
     // paper beats rock
 
-function getComputerChoice() {
+const computerSelection = getComputerChoice();
+const gameStart = document.querySelector('.gameStartButton');
 
+function getComputerChoice() {
     const choiceArray = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * choiceArray.length);
     const computerChoice = choiceArray[randomIndex].toLowerCase();
     return computerChoice;
 };
 
-// single round of RPS
 function playRound (playerSelection, computerSelection) {
 
     playerSelection = playerSelection.toLowerCase();
-
 
     if (computerSelection == 'paper') {
         if (playerSelection == 'scissors') {
@@ -52,10 +52,81 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerSelection, computerSelection);
+function game() {
 
+    let playerPoints = 0;
+    let computerPoints = 0;
+    let whichRound = 0;
 
+    for (let i = 0; i <= 5; i++) {
+
+        // if fifth and final round then declare winner
+        if (whichRound == 5) {
+
+            console.log(`Round ${whichRound}!
+                The final score is:
+                You: ${playerPoints}
+                Computer: ${computerPoints}`);
+
+            if (computerPoints > playerPoints) {
+                console.log('You lost the game :(');
+                return;
+            }
+            else if (playerPoints > computerPoints) {
+                console.log('You won the game :)');
+                return;
+            }
+            else if (playerPoints == computerPoints) {
+                console.log('The game is tied!');
+                return;
+            }
+        }
+
+        if (whichRound == 0) {
+            console.log(`Let's play!
+            The current score is:
+            You: ${playerPoints}
+            Computer: ${computerPoints}`);
+        }
+        
+        else if (whichRound >= 1) { 
+            console.log(`Round ${whichRound}!
+                    The current score is:
+                    You: ${playerPoints}
+                    Computer: ${computerPoints}`);
+        }
+
+        // get user input via window prompt
+        let playerSelection = prompt('Please enter rock, paper, or scissors:').toLowerCase();
+
+        if (playerSelection === 'rock' 
+            || (playerSelection) === 'paper' 
+            || (playerSelection) === 'scissors') {
+
+                let str = playRound(playerSelection, computerSelection);
+
+                if (str.includes('Win')) {
+                    playerPoints ++;
+                    whichRound ++;
+                    continue;
+                }
+                else if (str.includes('Lose')) {
+                    computerPoints ++;
+                    whichRound ++;
+                    continue;
+                }
+                else if (str.includes('Tied')) {
+                    whichRound ++;
+                    console.log(`This round ${whichRound} is tied. Score remains the same.`)
+                    continue;
+                }
+        }
+        else {
+            prompt('Incorrect entry. Please enter rock, paper, or scissors:').toLowerCase();
+            continue;
+        }
+    } 
+}
+
+gameStart.addEventListener('click', game);
 
